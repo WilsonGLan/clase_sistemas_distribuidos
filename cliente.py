3bfbcffa2c7bcd09a9d0790859fb5ed93520ebf7
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-03-25 22:10:02 -0500

    Solicitando al usuario 5 veces los datos

--- cliente.py
+++ cliente.py
@@ -1,10 +1,20 @@
 import socket
 
-socket_client = socket.socket()
-socket_client.connect(('localhost', 8000))
+for _ in range(5):
+    socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_client.connect(('localhost', 8000))
+    datos_cuenta = input("Por favor ingresar la cuenta: ")
+    datos_monto = input("Por favor ingresar el valor: ")
+    datos_ingresar = datos_cuenta+","+datos_monto
+    print(datos_ingresar)
+    bytes_datos = str.encode(datos_ingresar) #Se pasa a bytes
+    print(bytes_datos)
+    socket_client.sendall(bytes_datos)
+    resultado = socket_client.recv(1024) # Buffer
+    socket_client.close()
+    print(resultado)
+    
 
-socket_client.sendall(b"Mensaje enviado del cliente")
-resultado = socket_client.recv(1024) # Buffer
 
-print(resultado)
-socket_client.close()
+
+
