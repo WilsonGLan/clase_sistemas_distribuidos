22d63fe87603370c673f4e73e5ff1fb708b1458b
Wilson Gutierrez Lancheros <wilsongutierrezl@gmail.com>
2021-03-25 19:44:45 -0500

    Comentarios para explicar el código

--- servidor.py
+++ servidor.py
@@ -1,6 +1,6 @@
 import socket
 
-socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Se declaran los sockets (familia, tipo)
 socket_server.bind(('localhost', 8000)) # Establezco la conexión en la máquina local con el puerto 8000
 socket_server.listen(5) #Cantidad de peticiones en cola
 
@@ -9,10 +9,10 @@ while True:
     conexion, direccion = socket_server.accept() # Aceptamos las peticiones
     print("La conexión se ha establecido en el servidor")
     print(direccion)
-    conexion.sendall(b"Mensaje enviado desde el servidor")
+    conexion.sendall(b"Mensaje enviado desde el servidor") # Enviamos el mensaje mediante la conexión
 
-    resultado = conexion.recv(1024) # Buffer
-    resultado = resultado.decode("utf-8")
+    resultado = conexion.recv(1024) # Buffer --> se recibe el mensaje del cliente y se almacena en variable
+    resultado = resultado.decode("utf-8") # Convertir tipo bytes a tipo cadena
     print(resultado)
 
     conexion.close()
